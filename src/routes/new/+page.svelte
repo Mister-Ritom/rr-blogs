15b178c6fd1e6dc4c90c450b6e9ea1a3e440fee2
Ritom <ritomghosh856@gmail.com>
2023-08-05 10:40:48 +0530

    Added html support

--- src/routes/new/+page.svelte
+++ src/routes/new/+page.svelte
@@ -3,6 +3,7 @@
 	import { auth, db } from "$lib/firebase";
 	import { doc, setDoc } from "firebase/firestore";
 	import { userStore } from "sveltefire";
+    import DOMPurify from "dompurify";
 
     let text: string;
     let titleText: string
@@ -16,7 +17,7 @@
     function uploadBlog(e: Event) {
         e.preventDefault()
         if(text!=undefined&&text!=null&&titleText!=undefined&&titleText!=null) {
-            const t = text
+            const t = DOMPurify.sanitize(text, { USE_PROFILES: { html: true } })
             const tt = titleText
             text = ""
             titleText = ""
@@ -38,11 +39,16 @@
     }
 </script>
 
+<svelte:head>
+	<title>New blog</title>
+	<meta name="description" content="Create a new blog in RR-Blogs and earn money" />
+</svelte:head>
+
 <form on:submit={uploadBlog}>
     <label for="title">Title</label>
     <input bind:value={titleText} type="text" id="title">
     <label for="paragraph">Paragraph</label>
-    <textarea bind:value={text} name="paragraph" id="paragraph" cols="20" rows="35"></textarea>
+    <textarea placeholder="Write only text or html code starting with <article>, ending with </article>" bind:value={text} name="paragraph" id="paragraph" cols="20" rows="35"></textarea>
     <button type="submit">Upload</button>
 </form>
 
--- src/routes/post/+page.svelte
+++ src/routes/post/+page.svelte
@@ -2,14 +2,21 @@
     import { page } from '$app/stores';
 	import { db } from '$lib/firebase';
 	import { docStore } from 'sveltefire';
+
     const url = $page.url;
     const id = url.searchParams.get("id")
     const post = docStore(db,`blogs/${id}`)
+
 </script>
+
+<svelte:head>
+    <title>{$post?.title??"RR-Blogs"}</title>
+    <meta name="description" content={`A blog of RR-Blogs about ${$post?.title??""}`}>
+</svelte:head>
 
 {#if !id}
     <h1>No Blog found</h1>
 {:else}
- <h1>{$post?.title}</h1>
- <p>{$post?.paragraph}</p>
+ <h1>{$post?.title?? ""}</h1>
+ <p>{@html $post?.paragraph?? ""}</p>
 {/if}
